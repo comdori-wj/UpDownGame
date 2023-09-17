cdd0a7e217133ebb769608ca5cecbbb72a371fc6
comdori <02145s1@gmail.com>
2023-09-18 00:32:57 +0900

    5-3. 값을 비교해보기

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class ViewController: UIViewController { // 클래스 안에 들어있는 함수 매서드 Method
     
-    @IBOutlet weak var slider: UISlider!
+    private var randomValue: Int = 0
+    private var tryCount: Int = 0
+    @IBOutlet weak private var slider: UISlider!
+    @IBOutlet weak private var tryCountLabel: UILabel!
+    @IBOutlet weak private var sliderValueLabel: UILabel!
+    @IBOutlet weak private var minimumValueLabel: UILabel!
+    @IBOutlet weak private var maximumValueLable: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,10 +28,21 @@ class ViewController: UIViewController { // 클래스 안에 들어있는 함수
     
     @IBAction func sliderValueChanged( _ sender: UISlider) {
         print(sender.value)
+        let integerValue: Int = Int(sender.value)
+        sliderValueLabel.text = String(integerValue)
     }
     
     @IBAction func touchUpHitButton(_ sender: UIButton) {
         print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        slider.value = Float(hitValue)
+        
+        tryCount = tryCount + 1
+        tryCountLabel.text = "\(tryCount) / 5" // 괄호안에 넣는 것을 문자열 보관법 이라함.
+        
+        if randomValue == hitValue {
+            print("YOU HIT!!")
+        }
     }
     
     @IBAction func touchUpResetButton(_ sender: UIButton) {
@@ -35,6 +52,16 @@ class ViewController: UIViewController { // 클래스 안에 들어있는 함수
     
     private func reset() {
         print("reset!")
+        randomValue = Int.random(in: 0...30) // ... -> 범위 연산자 일종
+        print(randomValue)
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLabel.text = "0"
+        maximumValueLable.text = "30"
+        sliderValueLabel.text = "15"
     }
 
 
