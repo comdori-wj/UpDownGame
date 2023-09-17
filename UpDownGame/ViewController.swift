e9c03f3c420149e0e4db833511032b8ba2ad7226
comdori <02145s1@gmail.com>
2023-09-17 23:22:03 +0900

    4-3. [도전!] 함수 만들어보기

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController { // 클래스 안에 들어있는 함수 매서드 Method
     
     @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        slider.setThumbImage(UIImage(named: "slider_thumb"), for: .normal)
+        
+        reset()
     }
     
     @IBAction func sliderValueChanged( _ sender: UISlider) {
@@ -26,6 +30,11 @@ class ViewController: UIViewController {
     
     @IBAction func touchUpResetButton(_ sender: UIButton) {
         print("touch up reset Button")
+        reset()
+    }
+    
+    private func reset() {
+        print("reset!")
     }
 
 
